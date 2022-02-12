cb94072ee8f8b77e8e0fb23998d06bf42b4ca9ef
fu-qingyun <1366773912@qq.com>
2022-02-12 14:33:11 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,132 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int day=0 ;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//		printf("星期一\n");
+//		break;
+//	case 2:
+//		printf("星期二\n");
+//		break;
+//	default:
+//		printf("输入错误");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	if (i==0)
+//		printf("112");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int line = 0;
+//	while (line < 50000)
+//	{
+//		printf("写代码：%d\n", line);
+//		line++;
+//	}
+//	if (line == 50000)
+//	{
+//		printf("我秃了");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int sum = 0;
+//	int a = 0;
+//	for (; a <= 100; a++)
+//	{
+//		sum = sum + a;
+//	}
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	do
+//	{
+//		printf("%d ", i);
+//		i++;
+//	} while (i <= 10);
+//	return 0;
+//}
+
+//#include<stdlib.h>
+//#include<string.h>
+//int main()
+//{
+//	char input[20] = { 0 };
+//	system("shutdown -s -t 60");
+//
+//again:
+//	printf("电脑将在60s内关机，输入“我是猪”即可终止关机\n");
+//	scanf("%s", input);
+//	if (strcmp(input, "我是猪")==0)//两个字符串之间比较不能用‘==’
+//	{
+//		system("shutdown -a");
+//	}
+//	else
+//	{
+//		goto again;
+//	}
+//	return 0;
+//}
+
+//#include <string.h>
+//int main()
+//{
+//	char arr[80];
+//	strcpy(arr, "kenobi is my");
+//	strcat(arr, "life");
+//	strcat(arr, "my heart");
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//#include<memory.h>
+//int main()
+//{
+//	char arr[] = "Hello xiaofu";
+//	memset(arr, '*', 4);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//#include<math.h>
+//int main()
+//{
+//	int i = 25;
+//	int x=sqrt(i);
+//	printf("x=%d\n", x);
+//	return 0;
+//}
+
+//#include<string.h>
+//int main()
+//{
+//	char arr1[] = { 0 };
+//	char arr2[] = "wsf";
+//	if (strcmp(arr1, arr2))
+//	{
+//		printf("NO!");
+//	}
+//	else
+//	{
+//		printf("YES!");
+//	}
+//	return 0;
+//}
+
